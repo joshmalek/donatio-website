36a2238f73c55a2c61545a925f307d0301d291cf
Abdul <muiz.rpi@gmail.com>
2020-06-18 11:20:17 -0400

    added url parsing for params

--- src/components/TwitterAuth.js
+++ src/components/TwitterAuth.js
@@ -1,8 +1,33 @@
 import React, { useEffect } from 'react'
 import Twitter from 'twitter-lite'
 import axios from 'axios'
+import qs from 'qs'
+import QueryString from 'query-string'
 
 const TwitterAuthCallback = () => {
+
+    useEffect(() => {
+        // process the oauth_token & oauth_verifier in the
+        // url pararms.
+
+        let parsed_params = QueryString.parse(window.location.search)
+        console.log(`Parsed params`)
+        console.log(parsed_params)
+
+        // axios({
+        //     method: 'POST',
+        //     url: 'https://api.twitter.com/oauth/access_token',
+        //     data: qs.stringify({
+        //         oauth_consumer_key: "ElvTnb0OJ3J9DSF9cCI3HZXTl", // TODO HIDE THIS!
+        //         oauth_token: ,
+        //         oauth_verifier: 
+        //     }),
+        //     headers: {
+        //         'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        //     }
+        // })
+
+    }, []);
 
     return (<div>Twitter auth callback!</div>)
 }
