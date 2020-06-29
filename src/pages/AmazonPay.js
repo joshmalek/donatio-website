232bd26d7a0090884147a491428030817f126f80
Muiz Yusuff <muiz.rpi@gmail.com>
2020-06-29 16:48:21 -0400

    Added amazon pay params parse

--- src/pages/AmazonPay.js
+++ src/pages/AmazonPay.js
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
 // import AmazonPay from "amazon-pay-react";
+import QueryString from "query-string";
 
 const AmazonPayReturn = () => {
+  useEffect(() => {
+    let parsed_params = QueryString.parse(window.location.search);
+    console.log(`Return Params:`);
+    console.log(parsed_params);
+  }, []);
+
   return <div>Amazon Pay Return!</div>;
 };
 
