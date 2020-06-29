1232bcfcd53b718ac33f2fce78fd56dc431a4a5b
Muiz Yusuff <muiz.rpi@gmail.com>
2020-06-29 07:16:56 -0400

    reimplemented amazon pay

--- src/pages/AmazonPay.js
+++ src/pages/AmazonPay.js
@@ -1,51 +1,19 @@
 import React, { useEffect, useState } from "react";
 import HtmlToReactParser from "html-to-react";
-import AmazonPay from "amazon-pay-react";
-
-const AmazonPayTest = () => {
-  const [billingAgreement, setBillingAgreement] = useState(null);
-  return (
-    <div>
-      <AmazonPay
-        clientId="amzn1.application-oa2-client.659d839c0bde45ec92f518eeec30cb96"
-        sellerId="A2PNSD9U3NZBP4"
-        agreementType={"BillingAgreement"}
-        scope="profile payments:widget"
-        btnType="PwA"
-        btnColor="Gold"
-        btnSize="medium"
-        onConsentChange={(hasConsent) => {}}
-        handleBillingAgreementId={(billingAgreementId) => {}}
-        billingAgreementId={billingAgreement}
-        useAmazonAddressBook={true}
-        isSandbox
-      />
-    </div>
-  );
-};
+// import AmazonPay from "amazon-pay-react";
 
 const AmazonPayReturn = () => {
   return <div>Amazon Pay Return!</div>;
 };
 
-export { AmazonPayTest, AmazonPayReturn };
+const AmazonPayTest = () => {
+  useEffect(() => {
+    // Setup Amazon Pay.
 
-{
-  /* <script
-        async
-        src="https://static-na.payments-amazon.com/OffAmazonPayments/us/sandbox/js/Widgets.js%22%3E"
-      ></script>
-      <div
-        data-ap-widget-type="expressPaymentButton"
-        data-ap-signature="9ic1gmWzYAO28xmBYBDLORVfGdXouD%2BoAc%2F2E7fFMgk%3D"
-        data-ap-seller-id="A2PNSD9U3NZBP4"
-        data-ap-access-key="AKIAJ5UW7IIODOHZSZKQ"
-        data-ap-lwa-client-id="amzn1.application-oa2-client.659d839c0bde45ec92f518eeec30cb96"
-        data-ap-return-url="http://donatio-site.herokuapp.com/amazonpay"
-        data-ap-currency-code="USD"
-        data-ap-amount="25"
-        data-ap-note=""
-        data-ap-shipping-address-required="false"
-        data-ap-payment-action="Authorize"
-      ></div> */
-}
+    window.showButton();
+  }, []);
+
+  return <div id="AmazonPayButton"></div>;
+};
+
+export { AmazonPayTest, AmazonPayReturn };
