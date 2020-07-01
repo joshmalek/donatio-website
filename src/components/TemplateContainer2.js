a6ab2b87408f19afb96234b403b5e7576ffa462f
Muiz Yusuff <muiz.rpi@gmail.com>
2020-07-01 05:53:29 -0400

    Added padding to container

--- src/components/TemplateContainer2.js
+++ src/components/TemplateContainer2.js
@@ -4,8 +4,10 @@ import Navbar from "./Navbar";
 const TemplateContainer2 = ({ children }) => {
   return (
     <div className="donatio-container-two">
-      <Navbar />
-      <div className="body-contents">{children}</div>
+      <div className="donatio-center-padd">
+        <Navbar />
+        <div className="body-contents">{children}</div>
+      </div>
     </div>
   );
 };
