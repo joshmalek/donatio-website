0f8a95dc2451019e108c0eb49b913b98e8aa6c26
Muiz Yusuff <muiz.rpi@gmail.com>
2020-07-01 00:36:39 -0400

    Made changes to twitter auth

--- src/components/TwitterAuth.js
+++ src/components/TwitterAuth.js
@@ -1,57 +1,39 @@
-import React, { useEffect } from 'react'
-import Twitter from 'twitter-lite'
-import axios from 'axios'
-import QueryString from 'query-string'
+import React, { useEffect } from "react";
+import Twitter from "twitter-lite";
+import axios from "axios";
+import QueryString from "query-string";
 
 const TwitterAuthCallback = () => {
+  useEffect(() => {
+    // process the oauth_token & oauth_verifier in the
+    // url pararms.
 
-    useEffect(() => {
-        // process the oauth_token & oauth_verifier in the
-        // url pararms.
+    let parsed_params = QueryString.parse(window.location.search);
 
-        let parsed_params = QueryString.parse(window.location.search)
+    axios
+      .post("http://localhost:4000/graphql", {
+        query: `query { processTwitterAuth (oauth_token: "${parsed_params.oauth_token}", oauth_verifier: "${parsed_params.oauth_verifier}") }`,
+      })
+      .then((res) => {
+        console.log(`Twitter API returned:`);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(`Error making Twitter API call`);
+        console.log(err);
+      })
+      .finally(() => {
+        window.close();
+      });
+  }, []);
 
-        axios.post("http://localhost:4000/graphql", {
-            'query': `query { processTwitterAuth (oauth_token: "${parsed_params.oauth_token}", oauth_verifier: "${parsed_params.oauth_verifier}") }`
-        })
-        .then(res => {
-            console.log(`Twitter API returned:`)
-            console.log(res)
-        })
-        .catch(err => {
-            console.log(`Error making Twitter API call`)
-            console.log(err)
-        })
-        .finally(() => {
-            window.close ()
-        })
-
-    }, []);
-
-    return (<div>Twitter auth callback!</div>)
-}
+  return <div>Twitter auth callback!</div>;
+};
 
 const TwitterAuth = () => {
+  useEffect(() => {}, []);
 
-    useEffect (() => {
+  return <div>Twitter Auth Setup!</div>;
+};
 
-        console.log(`Configuting twitter-lite`)
-        // const twitterClient = new Twitter({
-        //     consumer_key: 'ElvTnb0OJ3J9DSF9cCI3HZXTl',
-        //     consumer_secret: '807do5gaUWt4q5WPZH4pvPOyGczFtyzRoEktlSbiJ0lFqSXcNM'
-        // })
-
-        // twitterClient.getRequestToken("https://donatio-site.herokuapp.com/twitter")
-        // .then(res => {
-        //     console.log(res)
-        // })
-        // .catch(err => {
-        //     console.log(err)
-        // })
-
-    }, [])
-
-    return (<div>Twitter Auth Setup!</div>)
-}
-
-export { TwitterAuth, TwitterAuthCallback }
+export { TwitterAuth, TwitterAuthCallback };
