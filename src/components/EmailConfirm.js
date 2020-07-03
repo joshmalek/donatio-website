ba4d3dc0a89a75ec0ab13be10f46abfe438ecc7d
Muiz Yusuff <muiz.rpi@gmail.com>
2020-07-02 22:53:41 -0400

    Fixed bad import

--- src/components/EmailConfirm.js
+++ src/components/EmailConfirm.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import "./styles.css";
 import { useAnimation, motion } from "framer-motion";
 
 export default function EmailConfirm() {
