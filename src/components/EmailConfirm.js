f5cfd6566837c943b59befe3f6f4f5658f0e958e
Muiz Yusuff <muiz.rpi@gmail.com>
2020-07-03 06:02:36 -0400

    Updated email confirm/password set style

--- src/components/EmailConfirm.js
+++ src/components/EmailConfirm.js
@@ -7,11 +7,24 @@ export default function EmailConfirm() {
   const emailController = useAnimation();
   const passwordController = useAnimation();
 
+  const [trueEmailValue, setTrueEmailValue] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+
+  useEffect(() => {
+    // Query the api to get the actual email value for the user
+    // and the user_id. Store them in trueEmailValue and userId
+    // respectively
+
+    setTrueEmailValue("sample@gmail.com");
+    setUserId("sample_user_id");
+  }, []);
+
   useEffect(() => {
     if (emailValue) {
       // HERE: Check if the email set is the valid email
       console.log(`Confirm Email: ${emailValue}`);
-      let email_valid = true;
+      let email_valid = emailValue === trueEmailValue;
 
       if (email_valid) {
         // hide the email container.
@@ -47,6 +60,7 @@ export default function EmailConfirm() {
         className="email-confirm-field-container"
       >
         <EmailConfirmation
+          email_invalid={emailValue != null && emailValue !== trueEmailValue}
           onConfirm={(email_value) => {
             setEmailValue(email_value);
           }}
@@ -57,10 +71,13 @@ export default function EmailConfirm() {
         className="password-confirm-field-container"
       >
         <PasswordSet
+          passwords_match={passwordsMatch}
           onConfirm={(password_, confirm_password) => {
             console.log(`${password_} == ${confirm_password}`);
             if (password_ === confirm_password) {
               setPasswordValue(password_);
+            } else {
+              setPasswordsMatch(false);
             }
           }}
         />
@@ -69,45 +86,61 @@ export default function EmailConfirm() {
   );
 }
 
-const PasswordSet = ({ onConfirm }) => {
+const PasswordSet = ({ onConfirm, passwords_match }) => {
   const pwdRef = useRef(null);
   const pwdConfirmRef = useRef(null);
 
   return (
     <div className="password-confirm-center">
-      <div style={{ marginBottom: "15px" }}>Set pasword for your account</div>
+      <div style={{ marginBottom: "15px" }}>
+        Complete your account setup by creating a pasword for your account.
+      </div>
       <div className="field-label">New Password</div>
       <input ref={pwdRef} className="donatio-input-field" />
       <div className="field-label">Confirm Password</div>
       <input ref={pwdConfirmRef} className="donatio-input-field" />
-      <div style={{ textAlign: "right" }}>
-        <ActionButton
-          value="Set Password"
-          onclick={() => {
-            if (onConfirm)
-              onConfirm(pwdRef.current.value, pwdConfirmRef.current.value);
-          }}
-        />
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ color: "red", flexGrow: 1 }}>
+          {!passwords_match && "Passwords do not match"}
+        </div>
+        <div style={{ textAlign: "right" }}>
+          <ActionButton
+            value="Set Password"
+            onclick={() => {
+              if (onConfirm)
+                onConfirm(pwdRef.current.value, pwdConfirmRef.current.value);
+            }}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-const EmailConfirmation = ({ onConfirm }) => {
+const EmailConfirmation = ({ onConfirm, email_invalid }) => {
   const emailRef = useRef(null);
 
   return (
     <div className="email-confirm-field">
       <div className="email-confirm-center">
+        <div style={{ fontSize: "20px", marginBottom: "20px" }}>
+          You're almost done confirming your account! Enter your account email
+          below to complete the confirmation.
+        </div>
         <div className="field-label">Confirm Email</div>
         <input ref={emailRef} className="donatio-input-field" />
-        <div style={{ textAlign: "right" }}>
-          <ActionButton
-            onclick={() => {
-              if (onConfirm) onConfirm(emailRef.current.value);
-            }}
-            value="Confirm"
-          />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flexGrow: 1, color: "red" }}>
+            {email_invalid && "Invalid Email"}
+          </div>
+          <div>
+            <ActionButton
+              onclick={() => {
+                if (onConfirm) onConfirm(emailRef.current.value);
+              }}
+              value="Confirm"
+            />
+          </div>
         </div>
       </div>
     </div>
